8476e5e8b437a5fe563e2a69bf41f8babd89ef28
Dzides <idzigun@.ru>
2021-12-18 04:06:30 +0300

    added unreakeased section

--- js/main.js
+++ js/main.js
@@ -1,27 +1,61 @@
 $(function () {
 
-    $('.burger').on('click', function () {
-       $('.burger').toggleClass('burger--active');
-       $('.menu').toggleClass('menu--active');
-       $('body').toggleClass('lock');
+   $('.burger').on('click', function () {
+      $('.burger').toggleClass('burger--active');
+      $('.menu').toggleClass('menu--active');
+      $('body').toggleClass('lock');
 
-    });
+   });
 
-    $('.menu__link').on('click', function () {
-       $('.burger').removeClass('burger--active');
-       $('.menu').removeClass('menu--active');
-       $('body').removeClass('lock');
+   $('.menu__link').on('click', function () {
+      $('.burger').removeClass('burger--active');
+      $('.menu').removeClass('menu--active');
+      $('body').removeClass('lock');
 
-    });
+   });
 
-    $(window).scroll(function () {
-       if ($(document).scrollTop() > 100) {
-          $(".header__nav").addClass("header__nav--animate");
-       } else {
-          $(".header__nav").removeClass("header__nav--animate");
-       }
-    });
 
-   var mixer = mixitup('.portfolio__board');
+   $(".card__btn").on("click", function () {
+      $(this).toggleClass("card__btn--active");
+   });
 
+   //  $('.card__btn').hover(
+   //     function () {
+   //        $(this).addClass('card__btn--active');
+   //     },
+   //     function () {
+   //        $(this).removeClass('card__btn--active');
+   //     }
+   //  );
+
+   $(window).scroll(function () {
+      if ($(document).scrollTop() > 100) {
+         $(".header__nav").addClass("header__nav--animate");
+      } else {
+         $(".header__nav").removeClass("header__nav--animate");
+      }
+   });
+
+   var swiperCategory = new Swiper('.category__swiper', {
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 26,
+
+      navigation: {
+         nextEl: '.category__button--next',
+         prevEl: '.category__button--prev',
+      },
+
+   });
+   var swiperCategory = new Swiper('.unreleased__swiper', {
+      loop: true,
+      slidesPerView: 5,
+      spaceBetween: 25,
+
+      navigation: {
+         nextEl: '.unreleased__button--next',
+         prevEl: '.unreleased__button--prev',
+      },
+
+   });
 });
